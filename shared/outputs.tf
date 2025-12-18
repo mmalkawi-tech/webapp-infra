df14612f58c710a9189cbe707b1158211bab52f9
unknown <moath.malkawi@techconsulting.tech>
2025-12-17 21:13:52 -0500

    Fix ACR module outputs for shared state

--- shared/outputs.tf
+++ shared/outputs.tf
@@ -1,9 +1,9 @@
 output "acr_id" {
-  value = module.acr.acr_id
+  value = module.acr.id
 }
 
 output "acr_name" {
-  value = module.acr.acr_name
+  value = module.acr.name
 }
 
 output "acr_login_server" {
